044d3db0f4b65f5e0b7f5856ac5c7550b1639b17
Louay_SMCE <louayltoufa@gmail.com>
2026-09-16 15:49:02 +0200

    added reverse function

--- script.py
+++ script.py
@@ -17,4 +17,16 @@ def word_count(text):
 print(word_count("Inazuma Eleven"))
 print(word_count("Hello there "))
 
+def reverse(text):
+    L=[]
+    for elm in text:
+        L.append(elm)
+    N = L[::-1]
+    new_text = ""
+    for elm in N:
+        new_text = new_text + elm
+    return new_text
+
+print(reverse("hello there"))
+
 
