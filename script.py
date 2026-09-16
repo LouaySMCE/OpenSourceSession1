83b1bf7ab0248d22414ba9f7ad4e9e4786ed7055
Louay_SMCE <louayltoufa@gmail.com>
2026-09-16 15:32:32 +0200

    added word_count function but could try a better approach

--- script.py
+++ script.py
@@ -4,5 +4,13 @@ def character_count(text):
 
 print(character_count("Inazuma Eleven"))
 
+def word_count(text):
+    if text == "" or text ==" ": return 0
+    count = 1
+    for elm in text:
+        if elm == " ":
+            count+=1
+    return count
 
+print(word_count("Inazuma Eleven"))
 
