643ccfab3450145bc9b9ebb451937c82001a6307
Louay_SMCE <louayltoufa@gmail.com>
2026-09-18 10:03:06 +0200

    added docstrings

--- script.py
+++ script.py
@@ -1,10 +1,19 @@
-
+"""
+This library has some utils function to manipulate and analyse texts.
+"""
 def character_count(text):
+    """
+    This function takes a text (string) as input and returns the number of character in it
+    """
     return len(text)
 
+# Usage exemple
 print(character_count("Inazuma Eleven"))
 
 def word_count(text):
+    """
+    This function takes a text (String) as input and returns the number of words in it
+    """
     if text == "" or text ==" ": return 0
     count = 1
     for elm in text:
@@ -14,10 +23,14 @@ def word_count(text):
         count = count -1
     return count
 
+# Usage exemple
 print(word_count("Inazuma Eleven"))
 print(word_count("Hello there "))
 
 def reverse(text):
+    """
+    This function takes a text (string) as input and makes it backwards.
+    """
     L=[]
     for elm in text:
         L.append(elm)
@@ -27,11 +40,16 @@ def reverse(text):
         new_text = new_text + elm
     return new_text
 
+# Usage exemple
 print(reverse("hello there"))
 
 def capitalize_words(text):
+    """
+    This function takes a text (string) as input and capitalize all letters in it
+    """
     return text.upper()
 
+# Usage exemple
 print(capitalize_words("inazuma"))
 
 
